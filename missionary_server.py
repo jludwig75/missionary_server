7f785cabc1c71795d806adac953c75212e62f802
Jonathan Ludwig <jr.ludwig@gmail.com>
2019-12-11 15:14:48 -0700

    added more webserver properties

--- missionary_server.py
+++ missionary_server.py
@@ -8,22 +8,31 @@ import json
 
 
 class MissionaryServer(object):
-    def __init__(self, timezone_name, open_weather_map_key, weather_location):
-        self._missionary_tz = pytz.timezone(timezone_name)
-        self._weather = Weather(open_weather_map_key, weather_location)
+    def __init__(self, settings):
+        self._missionary_name = settings['missionary_name']
+        self._mission_name = settings['mission_name']
+        self._missionary_tz = pytz.timezone(settings['timezone'])
+        self._weather = Weather(settings['open_weather_map_key'], settings['location'])
 
     @property
     def _local_time(self):
         return datetime.now()
 
-    @property
-    def _pht_time(self):
-        utc_now = pytz.utc.localize(datetime.utcnow())
+    def _mission_time(self, t=None):
+        utc_now = pytz.utc.localize(datetime.utcnow() if t == None else t)
         return utc_now.astimezone(self._missionary_tz)
 
     @cherrypy.expose
     def index(self):
         return "Hello world!"
+
+    @cherrypy.expose
+    def missionary_name(self):
+        return self._missionary_name
+
+    @cherrypy.expose
+    def mission_name(self):
+        return self._mission_name
 
     @cherrypy.expose
     def local_time(self):
@@ -36,19 +45,59 @@ class MissionaryServer(object):
         return now.strftime('%a, %b %d, %Y').lstrip("0").replace(" 0", " ")
 
     @cherrypy.expose
-    def missionary_time(self):
-        pht_now = self._pht_time
+    def mission_time(self):
+        pht_now = self._mission_time()
         return pht_now.strftime('%I:%M %p').lstrip("0").replace(" 0", " ")
 
     @cherrypy.expose
-    def missionary_date(self):
-        pht_now = self._pht_time
+    def mission_date(self):
+        pht_now = self._mission_time()
         return pht_now.strftime('%a, %b %d, %Y').lstrip("0").replace(" 0", " ")
+    
+    @cherrypy.expose
+    def mission_temp(self):
+        try:
+            cc = self._weather.current_conditions()
+            temp = '%.1f' % cc['main']['temp']
+        except:
+            temp = 'unavailable'
+        return temp
+    
+    @cherrypy.expose
+    def mission_humidity(self):
+        try:
+            cc = self._weather.current_conditions()
+            humidity = str(cc['main']['humidity'])
+        except:
+            humidity = 'unavailable'
+        return humidity
+    
+    @cherrypy.expose
+    def mission_sunrise(self):
+        try:
+            cc = self._weather.current_conditions()
+            sunrise = int(cc['sys']['sunrise'])
+            dt = datetime.fromtimestamp(sunrise, self._missionary_tz)
+            sunrise = dt.strftime('%I:%M %p').lstrip("0").replace(" 0", " ")
+        except:
+            sunrise = 'unavailable'
+        return sunrise
+    
+    @cherrypy.expose
+    def mission_sunset(self):
+        try:
+            cc = self._weather.current_conditions()
+            sunset = int(cc['sys']['sunset'])
+            dt = datetime.fromtimestamp(sunset, self._missionary_tz)
+            sunset = dt.strftime('%I:%M %p').lstrip("0").replace(" 0", " ")
+        except:
+            sunset = 'unavailable'
+        return sunset
 
 
 if __name__ == '__main__':
     with open('settings.json') as f:
         settings = json.loads(f.read())
     cherrypy.server.socket_host = '0.0.0.0'
-    cherrypy.quickstart(MissionaryServer(settings['timezone'], settings['open_weather_app_key'], settings['location']))
+    cherrypy.quickstart(MissionaryServer(settings))
     
