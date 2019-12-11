f56ff9938e06cf0c4bd7c6ad92a30eb997438b60
Jonathan Ludwig <jr.ludwig@gmail.com>
2019-12-11 16:25:05 -0700

    Hopefully finished now

--- missionary_server.py
+++ missionary_server.py
@@ -14,12 +14,42 @@ class Gender:
     Male = 0
     Female = 1
 
-class MissionaryServer(object):
+class Test(object):
+    @cherrypy.expose
+    def message(self):
+        return 'hello'
+
+
+def _user_date_format(dt):
+    # remove any leading pad 0's from date
+    return dt.strftime(USER_DATE_FORMAT).lstrip("0").replace(" 0", " ")
+
+def _user_time_format(dt):
+    # remove any leading pad 0's from time (except after :'s)
+    return dt.strftime(USER_TIME_FORMAT).lstrip("0").replace(" 0", " ")
+
+class Local(object):
+    def __init__(self, settings):
+        pass
+
+    def _local_time(self):
+        return datetime.now()
+
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+    @cherrypy.expose
+    def time(self):
+        return _user_time_format(self._local_time())
+
+    @cherrypy.expose
+    def date(self):
+        return _user_date_format(self._local_time())
+
+class Missionary(object):
     def __init__(self, settings):
         self._missionary_name = settings['missionary_name']
-        self._mission_name = settings['mission_name']
-        self._missionary_tz = pytz.timezone(settings['timezone'])
-        self._weather = Weather(settings['open_weather_map_key'], settings['location'])
 
         gender = settings['missionary_gender'].upper()
         if gender == 'M':
@@ -39,96 +69,26 @@ class MissionaryServer(object):
             days = 2 * 365 if self._gender == Gender.Male else 365 + 365 / 2
             self._release_date = self._start_date + timedelta(days=days)
 
-    def _local_time(self):
-        return datetime.now()
-
-    def _mission_time(self, t=None):
-        utc_now = pytz.utc.localize(datetime.utcnow() if t == None else t)
-        return utc_now.astimezone(self._missionary_tz)
-
-    def _user_date_format(self, dt):
-        # remove any leading pad 0's from date
-        return dt.strftime(USER_DATE_FORMAT).lstrip("0").replace(" 0", " ")
-
-    def _user_time_format(self, dt):
-        # remove any leading pad 0's from time (except after :'s)
-        return dt.strftime(USER_TIME_FORMAT).lstrip("0").replace(" 0", " ")
+    def _missionary_title(self):
+        return 'Elder' if self._gender == Gender.Male else 'Sister'
 
     @cherrypy.expose
     def index(self):
         return "Hello world!"
 
     @cherrypy.expose
-    def missionary_name(self):
-        return self._missionary_name
-
-    @cherrypy.expose
-    def mission_name(self):
-        return self._mission_name
-
-    @cherrypy.expose
-    def local_time(self):
-        return self._user_time_format(self._local_time())
-
-    @cherrypy.expose
-    def local_date(self):
-        return self._user_date_format(self._local_time())
-
-    @cherrypy.expose
-    def mission_time(self):
-        return self._user_time_format(self._mission_time())
-
-    @cherrypy.expose
-    def mission_date(self):
-        return self._user_date_format(self._mission_time())
-    
-    @cherrypy.expose
-    def mission_temp(self):
-        try:
-            cc = self._weather.current_conditions()
-            temp = '%.1f' % cc['main']['temp']
-        except:
-            temp = 'unavailable'
-        return temp
-    
-    @cherrypy.expose
-    def mission_humidity(self):
-        try:
-            cc = self._weather.current_conditions()
-            humidity = str(cc['main']['humidity'])
-        except:
-            humidity = 'unavailable'
-        return humidity
-    
-    @cherrypy.expose
-    def mission_sunrise(self):
-        try:
-            cc = self._weather.current_conditions()
-            sunrise = int(cc['sys']['sunrise'])
-            dt = datetime.fromtimestamp(sunrise, self._missionary_tz)
-            sunrise = dt.strftime('%I:%M %p').lstrip("0").replace(" 0", " ")
-        except:
-            sunrise = 'unavailable'
-        return sunrise
-    
-    @cherrypy.expose
-    def mission_sunset(self):
-        try:
-            cc = self._weather.current_conditions()
-            sunset = int(cc['sys']['sunset'])
-            dt = datetime.fromtimestamp(sunset, self._missionary_tz)
-            sunset = dt.strftime('%I:%M %p').lstrip("0").replace(" 0", " ")
-        except:
-            sunset = 'unavailable'
-        return sunset
+    def name(self, title=None):
+        if title == None:
+            return self._missionary_name
+        return '%s %s' % (self._missionary_title(), self._missionary_name)
 
     @cherrypy.expose
     def start_date(self):
-        return self._user_date_format(self._start_date)
+        return _user_date_format(self._start_date)
 
     @cherrypy.expose
     def release_date(self):
-        return self._user_date_format(self._release_date)
+        return _user_date_format(self._release_date)
 
     @cherrypy.expose
     def days_served(self):
@@ -146,9 +106,84 @@ class MissionaryServer(object):
         print(type(duration))
         return str(duration.days)
 
+class Mission(object):
+    def __init__(self, settings):
+        self._mission_name = settings['mission_name']
+        self._missionary_tz = pytz.timezone(settings['timezone'])
+        self._weather = Weather(settings['open_weather_map_key'], settings['location'])
+
+    def _mission_time(self, t=None):
+        utc_now = pytz.utc.localize(datetime.utcnow() if t == None else t)
+        return utc_now.astimezone(self._missionary_tz)
+
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+    @cherrypy.expose
+    def name(self):
+        return self._mission_name
+
+    @cherrypy.expose
+    def time(self):
+        return _user_time_format(self._mission_time())
+
+    @cherrypy.expose
+    def date(self):
+        return _user_date_format(self._mission_time())
+    
+    @cherrypy.expose
+    def temperature(self):
+        try:
+            cc = self._weather.current_conditions()
+            return '%.1f' % cc['main']['temp']
+        except:
+            return 'unavailable'
+    
+    @cherrypy.expose
+    def humidity(self):
+        try:
+            cc = self._weather.current_conditions()
+            return str(cc['main']['humidity'])
+        except:
+            return 'unavailable'
+    
+    @cherrypy.expose
+    def sunrise(self):
+        try:
+            cc = self._weather.current_conditions()
+            sunrise = int(cc['sys']['sunrise'])
+            dt = datetime.fromtimestamp(sunrise, self._missionary_tz)
+            return dt.strftime('%I:%M %p').lstrip("0").replace(" 0", " ")
+        except:
+            return 'unavailable'
+    
+    @cherrypy.expose
+    def sunset(self):
+        try:
+            cc = self._weather.current_conditions()
+            sunset = int(cc['sys']['sunset'])
+            dt = datetime.fromtimestamp(sunset, self._missionary_tz)
+            return dt.strftime('%I:%M %p').lstrip("0").replace(" 0", " ")
+        except:
+            return 'unavailable'
+
+    @cherrypy.expose
+    def time_of_day(self):
+        try:
+            cc = self._weather.current_conditions()
+            sunrise = int(cc['sys']['sunrise'])
+            sunset = int(cc['sys']['sunset'])
+            now = int(cc['dt'])
+            return "day" if now >= sunrise and now <= sunset else "night"
+        except:
+            return 'unavailable'
+
+
 if __name__ == '__main__':
     with open('settings.json') as f:
         settings = json.loads(f.read())
     cherrypy.server.socket_host = '0.0.0.0'
-    cherrypy.quickstart(MissionaryServer(settings))
-    
+    cherrypy.tree.mount(Local(settings), '/local')
+    cherrypy.tree.mount(Missionary(settings), '/missionary')
+    cherrypy.quickstart(Mission(settings), '/mission')
